1980e75076a75857f8dc4b6071bffc6803a8aa99
ben <noahbenaas@gmail.com>
2020-09-16 17:32:58 +0200

    just playing around

--- js/components/createHtml.js
+++ js/components/createHtml.js
@@ -10,6 +10,7 @@ export default function createHtml(data) {
     for (let i = 0; i < 7; i++) {
         resultContainer.innerHTML += `<div class="result">
                                         <h4>${data[i].name}</h4>
+                                       
                                         <p>Birthday: <b>${data[i].birthday}</b></p>
                                         <p>Nickname: <b>${data[i].nickname}</b></p>
                                         <div>Occupations:                                         
